efa3ceaff6d42213ba35a48a4f6b7a6fe2942d93
Tomáš Polák <polakto@gmail.com>
2023-02-06 22:26:46 +0100

    added class change to recalc actions

--- src/utils/saveable-actions-enum.js
+++ src/utils/saveable-actions-enum.js
@@ -1,5 +1,5 @@
-const reacalcActionsEnum = ["MOUSE_UP_RESIZE_BOX", "DELETE_REGION", "DELETE_GROUP", "MOUSE_UP_MOVE_REGION"]
+const reacalcActionsEnum = ["MOUSE_UP_RESIZE_BOX", "DELETE_REGION", "DELETE_GROUP", "MOUSE_UP_MOVE_REGION", "SELECT_CLASSIFICATION", "CHANGE_REGION"]
 
-const saveableActionsEnum = [reacalcActionsEnum, "SELECT_CLASSIFICATION", "CHANGE_REGION", "UPDATE_REGIONS", "UPDATE_METADATA", "UPDATE_ALBUM_METADATA"];
+const saveableActionsEnum = [reacalcActionsEnum, "UPDATE_REGIONS", "UPDATE_METADATA", "UPDATE_ALBUM_METADATA"];
 
 export { saveableActionsEnum, reacalcActionsEnum };
