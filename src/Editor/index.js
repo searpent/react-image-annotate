33c6feeab9fb28fbe9b0a9e8295f9e153a074bf5
michal <michal.juza@searpent.com>
2025-12-23 18:33:08 +0100

    additional toggle for spellcheck

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -3,11 +3,14 @@ import { createReactEditorJS } from 'react-editor-js'
 import blocksToArticle from '../utils/blocks-to-article';
 import { EDITOR_JS_TOOLS } from './tools'
 import ReadOnly from './readOnly';
+import { isSpellcheckEnabled, setSpellcheckEnabled } from '../utils/spellcheck-nspell'
+import './editor.css';
 
 const ReactEditorJS = createReactEditorJS()
 
 function Editor({ blocks = [], onChange, imageIndex, selectedFrame }) {
   const [editMode, setEditMode] = useState(false);
+  const [autoSpellcheck, setAutoSpellcheck] = useState(isSpellcheckEnabled());
   const handleChange = async instance => {
     const data = await instance.saver.save();
     onChange({ imageIndex, data })
@@ -16,18 +19,45 @@ function Editor({ blocks = [], onChange, imageIndex, selectedFrame }) {
     setEditMode(prev => !prev)
   }
 
+  const toggleAutoSpellcheck = () => {
+    setAutoSpellcheck(prev => {
+      const next = !prev
+      setSpellcheckEnabled(next)
+      return next
+    })
+  }
+
   if (blocks.length < 1) {
     return <div className='instructions'><h1>Click article to display text.</h1></div>
   }
 
   return (
     <div>
-      <div className="show-metadata-wrapper">
-        <label className="switch mr-2">
-          <input id="show-metadata" type="checkbox" value={editMode} onChange={toggleEditMode} />
-          <span className="slider round"></span>
-        </label>
-        <label>Edit mode</label>
+      <div className="show-metadata-wrapper editor-toggle-wrapper">
+        <div>
+          <label className="switch mr-2 editor-switch">
+            <input
+              id="automatic-spellcheck"
+              type="checkbox"
+              checked={autoSpellcheck}
+              onChange={toggleAutoSpellcheck}
+            />
+            <span className="slider round"></span>
+          </label>
+          <label className="mr-4">Spellcheck</label>
+        </div>
+        <div>
+          <label className="switch mr-2 editor-switch">
+            <input
+              id="show-metadata"
+              type="checkbox"
+              checked={editMode}
+              onChange={toggleEditMode}
+            />
+            <span className="slider round"></span>
+          </label>
+          <label>Edit mode</label>
+        </div>
       </div>
 
       {
